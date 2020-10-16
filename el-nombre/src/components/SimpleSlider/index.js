8e4539e5f7602f608017ab2f4cea169517025a1b
Marcelo Diament <marcelodiament@gmail.com>
2020-10-15 21:52:46 -0300

    feature: custom style [003]

--- el-nombre/src/components/SimpleSlider/index.js
+++ el-nombre/src/components/SimpleSlider/index.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import slides from './slides.json';
 import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// import "slick-carousel/slick/slick-theme.css";
+import './styles.css';
 
 export default class SimpleSlider extends Component {
   render() {
